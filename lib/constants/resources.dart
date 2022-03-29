8ee6f801b442c2e8522602969cb3a68d505b5972
Karen <kazem.ghorbanian@maxinai.com>
2022-03-29 17:36:25 +0300

    update the logo

--- lib/constants/resources.dart
+++ lib/constants/resources.dart
@@ -3,7 +3,7 @@ abstract class Resources {
   static const bikesJson = 'assets/bikes.json';
 
   //images
-  static const internetStoreLogo = 'assets/images/internetstore_logo.jpg';
+  static const internetStoreLogo = 'assets/images/bikes_logo.png';
   static const bikePlaceHolder = 'assets/images/bike_placeholder.png';
   static const emptyIndicator = 'assets/images/empty_indicator.png';
 }
